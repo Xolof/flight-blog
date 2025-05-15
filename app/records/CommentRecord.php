091f4ccebeab4750fd067b8fba496f0ecc19775b
Olof Johansson <olof@liberdev.se>
2025-05-15 10:44:10 +0200

    Create record for comments

---
+++ app/records/CommentRecord.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\records;
+
+/**
+ * ActiveRecord class for the comments table.
+ * @link https://docs.flightphp.com/awesome-plugins/active-record
+ *
+ * @property int $id
+ * @property int $post_id
+ * @property string $username
+ * @property string $content
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class CommentRecord extends \flight\ActiveRecord
+{
+    /**
+     * @var array $relations Set the relationships for the model
+     *   https://docs.flightphp.com/awesome-plugins/active-record#relationships
+     */
+    protected array $relations = [];
+
+    /**
+     * Constructor
+     * @param mixed $databaseConnection The connection to the database
+     */
+    public function __construct($databaseConnection)
+    {
+        parent::__construct($databaseConnection, 'comments');
+    }
+}
